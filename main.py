12a7eda4828579a0dfdd99d0101561860357dc64
G Roques <groques360@gmail.com>
2018-03-26 05:51:51 -0500

    Add loading indicator to output

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from bisector import Bisector
+from loading_indicator import LoadingIndicator
 
 
 def main():
     string_size = prompt_for_string_size()
+    loading_indicator = LoadingIndicator('Finding minimum population size', 1.0)
+    loading_indicator.start()
     minimum_population_size = Bisector.find_minimum_population_size(string_size)
-    print("Finding minimum population size", end='')
-    print("Minimum population size: {}".format(minimum_population_size))
+    loading_indicator.cancel()
+    print('Minimum population size: {}'.format(minimum_population_size))
 
 
-def prompt_for_string_size():
+def prompt_for_string_size() -> int:
+    """Get string size from user."""
     string_size = 0
     while string_size < 1:
         try:
             string_size = int(input("String size: "))
-        except ValueError as e:
+        except ValueError:
             pass
     return string_size
 
